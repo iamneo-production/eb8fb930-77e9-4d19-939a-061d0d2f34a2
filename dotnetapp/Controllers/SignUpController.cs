7305fd6bb9dc82a4001ebf5d49ff5f45d04ca337
avaningale <avan.ingale@gmail.com>
2023-04-29 11:25:04 +0000

    final commit

---
+++ dotnetapp/Controllers/SignUpController.cs
@@ -0,0 +1,49 @@
+﻿using dotnetapp.Core;
+using dotnetapp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+
+namespace dotnetapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        private readonly SignUpCore signUpCore;
+        public SignUpController(SignUpCore signUpCore)
+        {
+            this.signUpCore = signUpCore;
+        }
+
+        [HttpPost]
+        [Route("SignUp")]
+        public async Task<ActionResult<ResponseModel>> SaveUser([FromBody] UserModel user)
+        {
+            try
+            {
+                ResponseModel responseModel = new ResponseModel();
+                if (user != null)
+                {
+                    var res = signUpCore.RegisterUser(user);
+                    responseModel.Response = res;
+                    responseModel.Status = true;
+                    responseModel.Message = "save sucessfully";
+                    return Ok(responseModel);
+                }
+                return Ok("Enter Proper Data");
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        
+    }
+}
---
+++ dotnetapp/Core/SignUpCore.cs
@@ -0,0 +1,56 @@
+﻿using dotnetapp.Context;
+using dotnetapp.Core.Interface;
+using dotnetapp.Models;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace dotnetapp.Core
+{
+    public class SignUpCore : ISignUp
+    {
+
+        private readonly LensContext context;
+       
+
+        public SignUpCore(LensContext context)
+        {
+            this.context = context;
+           
+        }
+
+
+        public string RegisterUser([FromBody] UserModel userModel)
+        {
+
+            try
+            {
+                
+                var response = context.UserTable.Add(userModel);
+                context.SaveChanges();
+                
+                if (response != null)
+                {
+                    return "Sucessfully Registered";
+
+                }
+                else
+                {
+                    return "Enter Proper Detail";
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+       
+    }
+}
