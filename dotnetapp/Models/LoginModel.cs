00d0d27c27f1253b96dab4b7b716729bc97ef673
sahadamigo <sahadahamed712@gmail.com>
2023-05-01 17:53:08 +0000

    final commit

---
+++ dotnetapp/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using dotnetapp.Context;
+
+namespace dotnetapp.Models
+{
+    public class LoginModel
+    {
+       
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+
+    }
+}
