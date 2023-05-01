378628628c06bbbb9bbeb83e72d555b549dc3422
HariniVenkat07 <hariniv372001@gmail.com>
2023-05-01 17:39:53 +0000

    final commit

---
+++ dotnetapp/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnetapp.Models
+{
+    public class ResponseModel
+    {
+        public dynamic response { get; set; }
+        public string Message { get; set; }
+
+        public bool? Status { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+
+    }
+}
